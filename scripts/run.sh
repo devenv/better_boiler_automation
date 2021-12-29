a3753a0b493d52e86249179a19133885a4549495
Boris Churzin <essl.main@gmail.com>
2021-12-30 01:45:24 +0200

    fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,4 +8,4 @@ cd boiler
 export PYTHONPATH=/home/pi/venv/lib/python3.7/site-packages:.
 export DD_TRACE_ENABLED=True
 export STATS_ENABLED=True
-ddtrace-run python3.7 -m run.py
+ddtrace-run python3.7 run.py
