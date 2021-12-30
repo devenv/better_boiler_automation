893807279654c7d54178f6aa71cab3833c25f11d
Boris Churzin <essl.main@gmail.com>
2021-12-30 16:18:58 +0200

    more efficient and next schedule gauge

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -21,23 +21,29 @@ class Scheduler:
             weather = self.weather_provider.get_weather_data()
             now = datetime.now()
             is_on = self.boiler_controller.is_on()
-            needs_to_be_on = False
-            needs_to_be_on_time = None
             metrics.gauge("schedules loaded", len(self.config.times))
-            for time in self.config.times:
-                with tracer.trace("schedule calculation"):
-                    hours_to_heat = self.calculator.needed_hours_to_heat(weather, time.intencity)
-                    if now + timedelta(hours=hours_to_heat) >= self._find_next_hour(time.hour, time.minute):
-                        needs_to_be_on = True
-                        needs_to_be_on_time = time
-            if not is_on and needs_to_be_on:
-                logger.info(f"Switching on for hour {needs_to_be_on_time.hour + self.config.TIME_ZONE}:{needs_to_be_on_time.minute} to heat {hours_to_heat:.2f}")
-                self.boiler_controller.turn_on()
-            elif is_on and not needs_to_be_on:
-                self.boiler_controller.turn_off()
+            with tracer.trace("schedule calculation"):
+                time = self._get_next_schedule()
+                metrics.gauge("next schedule", time.hour + self.config.TIME_ZONE + time.minute / 60)
+                hours_to_heat = self.calculator.needed_hours_to_heat(weather, time.intencity)
+                if now + timedelta(hours=hours_to_heat) >= self._find_next_hour(time):
+                    if not is_on:
+                        logger.info(f"Switching on for hour {time.hour + self.config.TIME_ZONE}:{time.minute} to heat {hours_to_heat:.2f}")
+                        self.boiler_controller.turn_on()
+                elif is_on:
+                    self.boiler_controller.turn_off()
 
-    def _find_next_hour(self, hour: int, minute: int):
+    def _get_next_schedule(self):
         now = datetime.now()
-        if now < datetime(now.year, now.month, now.day, hour, minute, 0):
-            return datetime(now.year, now.month, now.day, hour, minute, 0)
-        return datetime(now.year, now.month, now.day + 1, hour, minute, 0)
+        minimal_time = self.config.times[0]
+        for time in self.config.times:
+            next_time = self._find_next_hour(time)
+            if next_time - now < self._find_next_hour(minimal_time) - now:
+                minimal_time = time
+        return minimal_time
+
+    def _find_next_hour(self, time):
+        now = datetime.now()
+        if now < datetime(now.year, now.month, now.day, time.hour, time.minute, 0):
+            return datetime(now.year, now.month, now.day, time.hour, time.minute, 0)
+        return datetime(now.year, now.month, now.day + 1, time.hour, time.minute, 0)
--- test/test_scheduler.py
+++ test/test_scheduler.py
@@ -44,7 +44,7 @@ class TestScheduler(TestCase):
     def test_scheduler_should_turn_on(self):
         boiler_controller = BoilerControllerSpy(is_on=False)
 
-        self._get_scheduler(3.5, boiler_controller, 8).check()
+        self._get_scheduler(3.5, boiler_controller, [8]).check()
 
         self.assertEquals(boiler_controller.turned_on, 1)
         self.assertEquals(boiler_controller.turned_off, 0)
@@ -53,7 +53,7 @@ class TestScheduler(TestCase):
     def test_scheduler_should_turn_off(self):
         boiler_controller = BoilerControllerSpy(is_on=True)
 
-        self._get_scheduler(3.5, boiler_controller, 11).check()
+        self._get_scheduler(3.5, boiler_controller, [11]).check()
 
         self.assertEquals(boiler_controller.turned_on, 0)
         self.assertEquals(boiler_controller.turned_off, 1)
@@ -62,20 +62,40 @@ class TestScheduler(TestCase):
     def test_scheduler_should_turn_on_with_jump(self):
         boiler_controller = BoilerControllerSpy(is_on=False)
 
-        self._get_scheduler(3.5, boiler_controller, 1).check()
+        self._get_scheduler(3.5, boiler_controller, [1]).check()
 
         self.assertEquals(boiler_controller.turned_on, 1)
         self.assertEquals(boiler_controller.turned_off, 0)
 
+    @freeze_time(datetime(2021, 1, 1, 4, 30, 0))
+    def test_find_next_schedule(self):
+        boiler_controller = BoilerControllerSpy(is_on=False)
+
+        scheduler = self._get_scheduler(3.5, boiler_controller, range(23))
+
+        next_schedule = scheduler._get_next_schedule()
+        self.assertEquals(next_schedule.hour, 5)
+        self.assertEquals(next_schedule.minute, 0)
+
+    @freeze_time(datetime(2021, 1, 1, 23, 30, 0))
+    def test_find_next_schedule_warp(self):
+        boiler_controller = BoilerControllerSpy(is_on=False)
+
+        scheduler = self._get_scheduler(3.5, boiler_controller, [12, 23, 1])
+
+        next_schedule = scheduler._get_next_schedule()
+        self.assertEquals(next_schedule.hour, 1)
+        self.assertEquals(next_schedule.minute, 0)
+
     @freeze_time(datetime(2021, 1, 1, 5, 0, 0))
     def test_find_next_hour_easy(self):
         scheduler = Scheduler(None, None, None, None)
-        self.assertEquals(scheduler._find_next_hour(7, 30), datetime(2021, 1, 1, 7, 30, 0))
+        self.assertEquals(scheduler._find_next_hour(Time(7, 30, 10)), datetime(2021, 1, 1, 7, 30, 0))
 
     @freeze_time(datetime(2021, 1, 1, 5, 0, 0))
     def test_find_next_hour_with_jump(self):
         scheduler = Scheduler(None, None, None, None)
-        self.assertEquals(scheduler._find_next_hour(4, 20), datetime(2021, 1, 2, 4, 20, 0))
+        self.assertEquals(scheduler._find_next_hour(Time(4, 20, 10)), datetime(2021, 1, 2, 4, 20, 0))
 
     def _run_simulation(self, weather_data, times):
         weather_provider = MagicMock()
@@ -96,12 +116,12 @@ class TestScheduler(TestCase):
         self.assertEquals(boiler_controller.turned_on_times, [self._datetime_for_hour(n[0]) for n in executions])
         self.assertEquals(boiler_controller.turned_off_times, [self._datetime_for_hour(n[1]) for n in executions])
         
-    def _get_scheduler(self, hours_to_heat, boiler_controller, configured_hour):
+    def _get_scheduler(self, hours_to_heat, boiler_controller, configured_hours):
         calculator = MagicMock()
         calculator.needed_hours_to_heat = MagicMock(return_value=hours_to_heat)
         weather_provider = MagicMock()
         config = MagicMock()
-        config.times = [Time(hour=configured_hour, minute=0, intencity=0)]
+        config.times = [Time(hour=hour, minute=0, intencity=0) for hour in configured_hours]
         return Scheduler(weather_provider, calculator, boiler_controller, config)
 
 
