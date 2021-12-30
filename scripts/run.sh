e39a446581b24dbbb1a7fc9a611b5b83ca9c5a29
Boris Churzin <essl.main@gmail.com>
2021-12-30 03:07:54 +0200

    no tracing for now

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@ set -e
 cd boiler
 
 export PYTHONPATH=/home/pi/venv/lib/python3.7/site-packages:.
-export DD_TRACE_ENABLED=True
+export DD_TRACE_ENABLED=False
 export STATS_ENABLED=True
 export DD_SERVICE=scheduler
 
