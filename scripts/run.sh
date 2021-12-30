6555103c83ddba72c6fe019836c0bd6f0b96b1d4
Boris Churzin <essl.main@gmail.com>
2021-12-30 02:23:23 +0200

    run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,4 +8,6 @@ cd boiler
 export PYTHONPATH=/home/pi/venv/lib/python3.7/site-packages:.
 export DD_TRACE_ENABLED=True
 export STATS_ENABLED=True
+export DD_SERVICE=scheduler
+
 ddtrace-run python3.7 run.py
