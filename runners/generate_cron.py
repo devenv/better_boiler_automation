739a6bf16b0ec4860d4b6d9937286d07665eb54c
Boris Churzin <essl.main@gmail.com>
2022-01-08 15:55:00 +0200

    better crons

--- runners/generate_cron.py
+++ runners/generate_cron.py
@@ -1,10 +1,12 @@
 from modules import available_modules
 
 PRE_DEPLOY_SCHEDULE = "*/5 * * * *"
+DEPLOY_MODULES_SCHEDULE = "*/5 * * * *"
 GENERATE_CRON_SCHEDULE = "*/5 * * * *"
 
 if __name__ == "__main__":
-    print(f"{GENERATE_CRON_SCHEDULE} sh generate_cron.sh | crontab -")
-    print(f"{PRE_DEPLOY_SCHEDULE} sh pre_deploy.sh")
+    print(f"{GENERATE_CRON_SCHEDULE} sh generate_cron.sh | crontab - >> ~/generate_cron.log 2>&1")
+    print(f"{PRE_DEPLOY_SCHEDULE} sh pre_deploy.sh >> ~/pre_deploy.log 2>&1")
     for module in available_modules:
-        print(f"{module.SCHEDULE} sh deploy_module.sh {module.NAME} && sh run_module.sh {module.NAME}")
+        print(f"{DEPLOY_MODULES_SCHEDULE} sh deploy_module.sh {module.NAME} >> ~/deploy_module_{module.NAME}.log 2>&1")
+        print(f"{module.SCHEDULE} sh run_module.sh {module.NAME} >> run_module_{module.NAME} 2>&1")
