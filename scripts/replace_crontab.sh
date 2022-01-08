9743ce33d670d85fd733870350397f5d787e0358
Boris Churzin <essl.main@gmail.com>
2022-01-08 16:21:04 +0200

    fix

---
+++ scripts/replace_crontab.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+. venv/bin/activate
+
+cd boiler_ready
+
+export PYTHONPATH=/home/pi/venv/lib/python3.7/site-packages:.
+export STATS_ENABLED=True
+export DD_SERVICE=$name
+
+old_cron=$(crontab -l)
+schedule=$(python -m runners.generate_cron)
+if [ -z "$schedule" ]; then
+    echo "Wrong crontab: $schedule"
+    exit 1
+fi
+
+if ! echo $schedule | crontab; then
+    echo "Couldn't set cron, reverting: $schedule"
+    echo $old_cron | crontab -
+fi 
