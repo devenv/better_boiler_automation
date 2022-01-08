642fd2e00563aa4f672a20b66adaf68b85d8ca15
Boris Churzin <essl.main@gmail.com>
2022-01-08 15:51:10 +0200

    fix

--- runners/generate_cron.py
+++ runners/generate_cron.py
@@ -4,7 +4,7 @@ PRE_DEPLOY_SCHEDULE = "*/5 * * * *"
 GENERATE_CRON_SCHEDULE = "*/5 * * * *"
 
 if __name__ == "__main__":
-    print(f"{GENERATE_CRON_SCHEDULE} python generate_cron.py | crontab -")
+    print(f"{GENERATE_CRON_SCHEDULE} sh generate_cron.sh | crontab -")
     print(f"{PRE_DEPLOY_SCHEDULE} sh pre_deploy.sh")
     for module in available_modules:
         print(f"{module.SCHEDULE} sh deploy_module.sh {module.NAME} && sh run_module.sh {module.NAME}")
