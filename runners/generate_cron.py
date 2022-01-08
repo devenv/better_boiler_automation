f0e74b7612491f999685874d9dee67810dd6c01f
Boris Churzin <essl.main@gmail.com>
2022-01-08 16:29:38 +0200

    prettier cron

--- runners/generate_cron.py
+++ runners/generate_cron.py
@@ -9,4 +9,5 @@ if __name__ == "__main__":
     print(f"{PRE_DEPLOY_SCHEDULE} sh pre_deploy.sh >> ~/pre_deploy.log 2>&1")
     for module in available_modules:
         print(f"{DEPLOY_MODULES_SCHEDULE} sh deploy_module.sh {module.NAME} >> ~/deploy_module_{module.NAME}.log 2>&1")
+    for module in available_modules:
         print(f"{module.SCHEDULE} sh run_module.sh {module.NAME} >> run_module_{module.NAME} 2>&1")
