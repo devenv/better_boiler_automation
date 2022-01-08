0518785fe677e31feb7f39f03f70677fe069d9ab
Boris Churzin <essl.main@gmail.com>
2022-01-08 14:31:39 +0200

    better scripts

--- scripts/generate_cron.sh
+++ scripts/generate_cron.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-name=$1
-
 . venv/bin/activate
-cd $name
 
 export PYTHONPATH=/home/pi/venv/lib/python3.7/site-packages:.
 export STATS_ENABLED=True
 export DD_SERVICE=$name
 
-python runners/run_module.py $name
+python runners/generate_cron.py
