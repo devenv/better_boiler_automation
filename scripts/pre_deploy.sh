cd188f4dcdce85c9277ca0392f1bb3574e960850
Boris Churzin <essl.main@gmail.com>
2022-01-09 13:33:30 +0200

    pre deploy fix

--- scripts/pre_deploy.sh
+++ scripts/pre_deploy.sh
@@ -9,7 +9,7 @@
 
   rm -rf boiler_clone
   git clone git@github.com:devenv/better_boiler_automation.git boiler_clone
-  cp boiler/scheduler/calendar_config.json boiler_clone/scheduler/
+  cp boiler_ready/scheduler/calendar_config.json boiler_clone/scheduler/
 
   cd boiler_clone
   new_last_commit=$(git log --format="%H" -n 1)
@@ -19,10 +19,10 @@
 
   rm -rf better_boiler_automation_configs
   git clone git@github.com:devenv/better_boiler_automation_configs.git
-  mkdir ~/.boiler/secrets -p
-  rsync -avP --exclude=.git better_boiler_automation_configs/secrets/ ~/.boiler/secrets/
   mkdir boiler_clone/secrets -p
   rsync -avP --exclude=.git better_boiler_automation_configs/secrets/ boiler_clone/secrets/
+  mkdir ~/.boiler_ready/secrets -p
+  rsync -avP --exclude=.git better_boiler_automation_configs/secrets/ ~/.boiler_ready/secrets/
 
   if [ "$new_last_commit" = "$old_last_commit" ]; then
     echo "skipping deployment, same commits: $new_last_commit == $old_last_commit"
@@ -33,8 +33,8 @@
   cd boiler_clone
 
   #pip3.7 install https://www.piwheels.org/simple/grpcio/grpcio-1.38.1-cp37-cp37m-linux_armv6l.whl
-
-  if ! cmp requirements.txt ../boiler/requirements.txt >/dev/null 2>&1; then
+ 
+  if ! cmp requirements.txt ../boiler_ready/requirements.txt >/dev/null 2>&1; then
     echo "Installing requirements"
     pip3.7 install -r requirements.txt
     if [ $? -eq 0 ]; then
@@ -56,7 +56,7 @@
     return 1
   fi
 
-  if ! cmp calculator/calculator_config.json ../boiler/calculator/calculator_config.json >/dev/null 2>&1; then
+  if ! cmp calculator/calculator_config.json ../boiler_ready/calculator/calculator_config.json >/dev/null 2>&1; then
     echo 'Calculator configuration change'
   fi
 
