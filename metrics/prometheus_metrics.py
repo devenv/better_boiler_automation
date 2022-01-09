e07e57a6f3081d49751e93d66ed4eca430a65c17
Boris Churzin <essl.main@gmail.com>
2022-01-09 14:57:04 +0200

    text metrics

--- metrics/prometheus_metrics.py
+++ metrics/prometheus_metrics.py
@@ -1,38 +1,33 @@
 import __main__
+import os
 from typing import Dict
-from prometheus_client import CollectorRegistry, Gauge, Counter, push_to_gateway
+from prometheus_client import CollectorRegistry, Gauge, Counter, write_to_textfile
 
 from metrics.metrics_client import MetricsClient
 
-GATEWAY = 'localhost:9091'
 JOB_NAME = __main__.__file__
+GLOBAL_STORE = os.environ.get("GLOBAL_STORE") == 'True'
+if GLOBAL_STORE:
+    METRICS_FILE = f"{os.path.expanduser('~')}/.boiler/data/metrics.txt"
+else:
+    METRICS_FILE = "data/metrics.txt"
 
 
 class PrometheusMetrics(MetricsClient):
 
-    known_metrics = {}
-    registry = CollectorRegistry()
-
     def incr(self, metric: str, tags: Dict[str, str] = {}):
+        registry = CollectorRegistry()
         metric = metric.replace('.', '_')
-        if metric in self.known_metrics:
-            counter = self.known_metrics[metric]
-        else:
-            counter = Counter(metric, metric, labelnames=list(tags.keys()), registry=self.registry)
-            self.known_metrics[metric] = counter
+        counter = Counter(metric, metric, labelnames=list(tags.keys()), registry=registry)
         if tags:
             counter = counter.labels(tags)
         counter.inc()
-        push_to_gateway(GATEWAY, job=JOB_NAME, registry=self.registry)
+        write_to_textfile(METRICS_FILE, registry)
 
     def gauge(self, metric: str, value: float, tags: Dict[str, str] = {}):
-        metric = metric.replace('.', '_')
-        if metric in self.known_metrics:
-            gauge = self.known_metrics[metric]
-        else:
-            gauge = Gauge(metric, metric, labelnames=list(tags.keys()), registry=self.registry)
-            self.known_metrics[metric] = gauge
+        registry = CollectorRegistry()
+        gauge = Gauge(metric, metric, labelnames=list(tags.keys()), registry=registry)
         if tags:
             gauge = gauge.labels(tags)
         gauge.set(value)
-        push_to_gateway(GATEWAY, job=JOB_NAME, registry=self.registry)
+        write_to_textfile(METRICS_FILE, registry)
