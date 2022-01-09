e3335d2e8ac7a0826f1372b6a3f4764be4597f2e
Boris Churzin <essl.main@gmail.com>
2022-01-09 15:18:47 +0200

    global registry

--- metrics/prometheus_metrics.py
+++ metrics/prometheus_metrics.py
@@ -12,22 +12,27 @@ if GLOBAL_STORE:
 else:
     METRICS_FILE = "data/metrics.txt"
 
+registry = CollectorRegistry()
 
 class PrometheusMetrics(MetricsClient):
 
     def incr(self, metric: str, tags: Dict[str, str] = {}):
-        registry = CollectorRegistry()
         metric = metric.replace('.', '_')
-        counter = Counter(metric, metric, labelnames=list(tags.keys()), registry=registry)
+        if metric in registry._names_to_collectors:
+            counter = registry._names_to_collectors[metric]
+        else:
+            counter = Counter(metric, metric, labelnames=list(tags.keys()), registry=registry)
         if tags:
             counter = counter.labels(tags)
         counter.inc()
         write_to_textfile(METRICS_FILE, registry)
 
     def gauge(self, metric: str, value: float, tags: Dict[str, str] = {}):
-        registry = CollectorRegistry()
         metric = metric.replace('.', '_')
-        gauge = Gauge(metric, metric, labelnames=list(tags.keys()), registry=registry)
+        if metric in registry._names_to_collectors:
+            gauge = registry._names_to_collectors[metric]
+        else:
+            gauge = Gauge(metric, metric, labelnames=list(tags.keys()), registry=registry)
         if tags:
             gauge = gauge.labels(tags)
         gauge.set(value)
