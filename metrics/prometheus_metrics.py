3b8f4041ee26b9b4722a93a685c6abf51454b5fc
Boris Churzin <essl.main@gmail.com>
2022-01-09 14:32:13 +0200

    prometheus fix

--- metrics/prometheus_metrics.py
+++ metrics/prometheus_metrics.py
@@ -10,31 +10,29 @@ JOB_NAME = __main__.__file__
 
 class PrometheusMetrics(MetricsClient):
 
-    def __init__(self):
-        self.known_metrics = {}
+    known_metrics = {}
+    registry = CollectorRegistry()
 
     def incr(self, metric: str, tags: Dict[str, str] = {}):
-        registry = CollectorRegistry()
         metric = metric.replace('.', '_')
         if metric in self.known_metrics:
             counter = self.known_metrics[metric]
         else:
-            counter = Counter(metric, metric, labelnames=list(tags.keys()), registry=registry)
+            counter = Counter(metric, metric, labelnames=list(tags.keys()), registry=self.registry)
             self.known_metrics[metric] = counter
         if tags:
             counter = counter.labels(tags)
         counter.inc()
-        push_to_gateway(GATEWAY, job=JOB_NAME, registry=registry)
+        push_to_gateway(GATEWAY, job=JOB_NAME, registry=self.registry)
 
     def gauge(self, metric: str, value: float, tags: Dict[str, str] = {}):
-        registry = CollectorRegistry()
         metric = metric.replace('.', '_')
         if metric in self.known_metrics:
             gauge = self.known_metrics[metric]
         else:
-            gauge = Gauge(metric, metric, labelnames=list(tags.keys()), registry=registry)
+            gauge = Gauge(metric, metric, labelnames=list(tags.keys()), registry=self.registry)
             self.known_metrics[metric] = gauge
         if tags:
             gauge = gauge.labels(tags)
         gauge.set(value)
-        push_to_gateway(GATEWAY, job=JOB_NAME, registry=registry)
+        push_to_gateway(GATEWAY, job=JOB_NAME, registry=self.registry)
