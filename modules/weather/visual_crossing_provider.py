2e08901125063b5bcfc832e2b853d2c275bf470c
Boris Churzin <boris.churzin@fundbox.com>
2022-01-18 14:13:17 +0200

    solar energy from last hour instead of current

--- modules/weather/visual_crossing_provider.py
+++ modules/weather/visual_crossing_provider.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import requests
 
@@ -15,21 +16,28 @@ class WeatherProvider:
     HOURS_TO_LOOK_BACK = 8
 
     def get_weather_data(self) -> WeatherData:
-        visualcrossing_data = json.loads(requests.get(f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{self.location}?unitGroup=metric&include=current&key={self.visualcrossing_api_key}&contentType=json').text)
-        current_weather = self._visualcrossing_to_weather_data(visualcrossing_data['currentConditions'])
+        visualcrossing_data = json.loads(requests.get(f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{self.location}?unitGroup=metric&include=hours%2Ccurrent&key={self.visualcrossing_api_key}&contentType=json').text)
+        current_weather = self._visualcrossing_to_weather_data(visualcrossing_data)
         return current_weather
         
     def _visualcrossing_to_weather_data(self, weather: dict) -> WeatherData:
+        current_data = weather['currentConditions']
+        now = datetime.now()
+        date_now = now.strftime("%Y-%m-%d")
+        hour_now = now.strftime("%H:00:00")
+        today_data = [day for day in weather['days'] if day['datetime'] == date_now][0]
+        last_hour_data = [hour for hour in today_data['hours'] if hour['datetime'] == hour_now][0]
+
         return WeatherData(
-            temperature=weather['temp'],
-            clouds=weather['cloudcover'],
-            feels_like=weather['feelslike'],
-            visibility=weather['visibility'],
-            humidity=weather['humidity'],
-            precipitation=weather['precip'] or 0,
-            pressure=weather['pressure'],
-            wind_speed=weather['windspeed'],
-            wind_direction=weather['winddir'],
-            solar_energy=weather['solarenergy'] or 0,
-            solar_radiation=weather['solarradiation'] or 0,
+            temperature=current_data['temp'],
+            clouds=current_data['cloudcover'],
+            feels_like=current_data['feelslike'],
+            visibility=current_data['visibility'],
+            humidity=current_data['humidity'],
+            precipitation=current_data['precip'] or 0,
+            pressure=current_data['pressure'],
+            wind_speed=current_data['windspeed'],
+            wind_direction=current_data['winddir'],
+            solar_energy=last_hour_data['solarenergy'] or 0,
+            solar_radiation=last_hour_data['solarradiation'] or 0,
         )
